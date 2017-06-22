04b30176c48f2e61a10e257c873973e0cbd587b6
JStein92 <jonathanstein@live.com>
2017-06-22 12:57:14 -0700

    add canDivide function to achieve more DRY code

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,41 @@
-function getRange(userNumber){
+function playPingPong(userNumber){
   var numbers = [];
   for (var i = 0; i <= userNumber; i++) {
-
     numbers.push(i);
+    var index = numbers.indexOf(i);
 
-      if (numbers.indexOf(i)%5===0 && numbers.indexOf(i)%3===0){
-        numbers[i] = "pingpong";
-      }
-      else if (numbers.indexOf(i)%3===0){
-        numbers[i] = "ping";
-      }
-      else if(numbers.indexOf(i)%5===0){
-        numbers[i] = "pong";
-      }
-
-
+    if (canDivide(index,3) && !canDivide(index,15)){
+      numbers.splice(i,1,"ping");
+    }
+    else if(canDivide(index,5) && !canDivide(index,15)){
+      numbers.splice(i,1,"pong");
+    }
+    else if (canDivide(index,15)){
+      numbers.splice(i,1,"pingpong");
+    }
+    else {
+     //do nothing
+    }
   }
   numbers.splice(0,1);
   return numbers;
 }
 
+function canDivide(num, factor){
+  if (num%factor==0){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 $(function(){
 
   $("form").submit(function(e){
     e.preventDefault();
     var userInput = parseInt($("#input").val());
-    $("#output").text(getRange(userInput));
+    $("#output").text(playPingPong(userInput));
   });
 
 });
