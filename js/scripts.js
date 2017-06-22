570034879687a277ee6cad3366708e87c32ba738
JStein92 <jonathanstein@live.com>
2017-06-22 12:24:35 -0700

    add logic to check for each item in numbers array to see if divisible by 15, then 3, then 5 and replace with requested string

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
 function getRange(userNumber){
   var numbers = [];
-  for (var i = 1; i <= userNumber; i++) {
+  for (var i = 0; i <= userNumber; i++) {
+
     numbers.push(i);
+
+      if (numbers.indexOf(i)%5===0 && numbers.indexOf(i)%3===0){
+        numbers[i] = "pingpong";
+      }
+      else if (numbers.indexOf(i)%3===0){
+        numbers[i] = "ping";
+      }
+      else if(numbers.indexOf(i)%5===0){
+        numbers[i] = "pong";
+      }
+
+
   }
+  numbers.splice(0,1);
   return numbers;
 }
+
 
 $(function(){
 
