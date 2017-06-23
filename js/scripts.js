08acf12e5d4aa88990c5dc3ea04ae9262db1e8bb
JStein92 <jonathanstein@live.com>
2017-06-23 08:24:12 -0700

    change js functions to add and remove CSS classes instead of directly modifying css properties

--- js/scripts.js
+++ js/scripts.js
@@ -46,45 +46,69 @@ function canDivide(num, factor){
 
 $(function(){
 
-  $("form").submit(function(e){
+  //add color to counter subheaders and form group
+  $('ping,#pingCount').addClass('ping-background');
+  $('pong,#pongCount').addClass('pong-background')
+  $('pingPong,#pingPongCount').addClass('pingPong-background');
 
+  $("form").submit(function(e){
     e.preventDefault();
     $('#output').text("");
     var userInput = parseInt($("#input").val());
     playPingPong(userInput);
+
+    $('ping,pong,pingPong,number').addClass('grey-border');
+    $('ping').addClass('ping-background');
+    $('pong').addClass('pong-background')
+    $('pingPong').addClass('pingPong-background');
+    $('number').addClass('number-background');
+
     $("html, body").animate({ scrollTop: $(document).height() }, 1000); //slide to  bottom of page
   });
 
   $('#pingCount').hover(function(){
-    $('ping,#pingCount').css('background-color', 'white');
-    $('ping').css('border', '1px solid black');
+    $('ping,#pingCount').removeClass('ping-background')
+    $('ping,#pingCount').addClass('white-background');
+    $('ping').removeClass('grey-border');
+    $('ping').addClass('black-border');
   }, function(){
-    $('ping,#pingCount').css('background-color', '#ffe7c4');
-    $('ping').css('border', '1px solid grey');
+    $('ping,#pingCount').removeClass('white-background')
+    $('ping,#pingCount').addClass('ping-background');
+    $('ping').removeClass('black-border');
+    $('ping').addClass('grey-border');
   });
-
   $('#pongCount').hover(function(){
-    $('pong,#pongCount').css('background-color', 'white');
-    $('pong').css('border', '1px solid black');
+    $('pong,#pongCount').removeClass('pong-background')
+    $('pong,#pongCount').addClass('white-background');
+    $('pong').removeClass('grey-border');
+    $('pong').addClass('black-border');
   }, function(){
-    $('pong,#pongCount').css('background-color', '#bbf3f7');
-    $('pong').css('border', '1px solid grey');
+    $('pong,#pongCount').removeClass('white-background')
+    $('pong,#pongCount').addClass('pong-background');
+    $('pong').removeClass('black-border');
+    $('pong').addClass('grey-border');
   });
-
   $('#pingPongCount').hover(function(){
-    $('pingPong,#pingPongCount').css('background-color', 'white');
-    $('pingPong').css('border', '1px solid black');
+    $('pingPong,#pingPongCount').removeClass('pingPong-background')
+    $('pingPong,#pingPongCount').addClass('white-background');
+    $('pingPong').removeClass('grey-border');
+    $('pingPong').addClass('black-border');
   }, function(){
-    $('pingPong,#pingPongCount').css('background-color', '#fbd6fc');
-    $('pingPong').css('border', '1px solid grey');
+    $('pingPong,#pingPongCount').removeClass('white-background')
+    $('pingPong,#pingPongCount').addClass('pingPong-background');
+    $('pingPong').removeClass('black-border');
+    $('pingPong').addClass('grey-border');
   });
-
   $('.form-group').hover(function(){
-    $('.form-group,number').css('background-color', 'white');
-    $('number').css('border', '1px solid black');
+    $('number,.form-group').removeClass('number-background')
+    $('number,.form-group').addClass('white-background');
+    $('number').removeClass('grey-border');
+    $('number').addClass('black-border');
   }, function(){
-    $('.form-group,number').css('background-color', '#dbfce0');
-    $('number').css('border', '1px solid grey');
+    $('number,.form-group').removeClass('white-background')
+    $('number,.form-group').addClass('number-background');
+    $('number').removeClass('black-border');
+    $('number').addClass('grey-border');
   });
 
 });
